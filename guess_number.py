2cfb854b7b8e05251f1e2a7ddaf922503fc2ca5e
CawWoo <melodicraven@gmail.com>
2024-02-07 13:51:50 -0600

    Added a quit checker and a loop to allow user multiple inputs

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,24 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = (input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while guess != number:
+
+    if guess == 'q':
+        print(f'The number was {number}...')
+        break
+
+    guess = int(guess)
+    
+    if guess != number:
+        guess = (input('Nope! Try again! '))
+    else:
+        print("Congratulations! You guessed the right number.")
+        break
+
+   
+
+
 
 
