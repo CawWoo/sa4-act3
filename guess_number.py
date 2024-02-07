2ab96571fde9e860ecb96f330c9e90969bd5e9c3
CawWoo <melodicraven@gmail.com>
2024-02-07 14:08:18 -0600

    Added limited tries to get number right, and a guess counter

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,12 @@
 number = 10
+tries = 5
 
 print("I'm thinking of a number...")
 guess = (input("What number am I thinking of? "))
+tries -= 1
+print(f'You have {tries} guesses left')
 
-while guess != number:
+while guess != number and tries != 0:
 
     if guess == 'q':
         print(f'The number was {number}...')
@@ -13,9 +16,17 @@ while guess != number:
     
     if guess != number:
         guess = (input('Nope! Try again! '))
-    else:
+        tries -= 1
+        print(f'You have {tries} guesses left')
+
+    if guess == number:
         print("Congratulations! You guessed the right number.")
-        break
+
+print(f'Sorry, you ran out of tries. The number was {number}')
+
+
+    
+
 
    
 
